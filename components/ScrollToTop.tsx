a175f3b7a223ed02c59cc7ddcad7dfd7a4066442
rahul <ladumorrahul56@gmail.com>
2025-01-26 12:08:14 +0530

    doc: minor changes on readme doc

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence,motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { FaArrowUp } from "react-icons/fa";
 
